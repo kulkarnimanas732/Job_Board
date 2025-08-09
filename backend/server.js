fd31ac6895aed4951a76c262f02aa3f899074c84
kulkarnimanas732 <kulkarnimanas732@gmail.com>
2025-08-09 14:34:39 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,9 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 
+app.get("/", (req, res) => {
+  res.send("JobBoard Backend is running 🚀");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
